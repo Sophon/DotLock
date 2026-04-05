09cc51e54c70f815b2d7abfa2b01db674d879062
Le Duy Anh <lenduya@gmail.com>
2026-04-05 23:12:03 +0200

    feat (deadlock): wiki client

--- feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/DeadlockWikiClient.kt
+++ feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/DeadlockWikiClient.kt
@@ -1,4 +1,40 @@
 package io.github.sophon.deadlock
 
+import io.github.sophon.core.arch.EmptyResult
+import io.github.sophon.core.arch.Error
+import io.github.sophon.core.arch.Result
+import io.github.sophon.core.domain.model.Ability
+import io.github.sophon.core.domain.model.Hero
+import io.github.sophon.core.domain.model.Item
+
 interface DeadlockWikiClient {
+    suspend fun downloadAllData(): EmptyResult<Error>
+
+    suspend fun fetchHero(heroName: String): Result<Hero, Error>
+    suspend fun fetchAbility(abilityName: String): Result<Ability, Error>
+    suspend fun fetchAbilities(heroName: String): Result<List<Ability>, Error>
+    suspend fun fetchItem(itemName: String): Result<Item, Error>
+}
+
+
+internal class DeadlockWikiClientImpl(): DeadlockWikiClient {
+    override suspend fun downloadAllData(): EmptyResult<Error> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchHero(heroName: String): Result<Hero, Error> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchAbility(abilityName: String): Result<Ability, Error> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchAbilities(heroName: String): Result<List<Ability>, Error> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchItem(itemName: String): Result<Item, Error> {
+        TODO("Not yet implemented")
+    }
 }
