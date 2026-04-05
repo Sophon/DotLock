7b176e34ee975150f0f9713a391a8fab95613644
Le Duy Anh <lenduya@gmail.com>
2026-04-05 23:15:11 +0200

    feat (deadlock): URL constants

--- feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/Constants.kt
+++ feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/Constants.kt
@@ -1,2 +1,5 @@
 package io.github.sophon.deadlock
 
+internal const val URL_HERO = "https://deadlock.wiki/Data:HeroData.json?action=raw"
+internal const val URL_ABILITY = "https://deadlock.wiki/Data:AbilityData.json?action=raw"
+internal const val URL_ITEM = "https://deadlock.wiki/Data:ItemData.json?action=raw"
