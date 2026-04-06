f0540696da68e0ef0cd4f5f3a3a451f7edfcde7d
Le Duy Anh <lenduya@gmail.com>
2026-04-06 21:36:25 +0200

    refactor (deadlock): usecases map

--- feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/domain/DownloadAbilitiesUseCase.kt
+++ feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/domain/DownloadAbilitiesUseCase.kt
@@ -1,18 +1,18 @@
 package io.github.sophon.deadlock.domain
 
-import io.github.sophon.core.arch.WikiError
 import io.github.sophon.core.arch.Result
+import io.github.sophon.core.arch.WikiError
 import io.github.sophon.core.arch.map
 import io.github.sophon.core.arch.mapError
-import io.github.sophon.core.domain.model.Hero
+import io.github.sophon.core.domain.model.Ability
 import io.github.sophon.deadlock.data.DeadlockWikiDataSource
 import io.github.sophon.deadlock.data.mapper.toDomain
 
-internal class DownloadHeroesUseCase(
+internal class DownloadAbilitiesUseCase(
     private val source: DeadlockWikiDataSource,
 ) {
-    suspend fun invoke(): Result<List<Hero>, WikiError> {
-        return source.downloadHeroList()
+    suspend fun invoke(): Result<List<Ability>, WikiError> {
+        return source.downloadAbilityList()
             .map { map -> map.entries.map { it.toDomain() } }
             .mapError { it.toDomain() }
     }
--- feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/domain/DownloadItemsUseCase.kt
+++ feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/domain/DownloadItemsUseCase.kt
@@ -1,18 +1,18 @@
 package io.github.sophon.deadlock.domain
 
-import io.github.sophon.core.arch.WikiError
 import io.github.sophon.core.arch.Result
+import io.github.sophon.core.arch.WikiError
 import io.github.sophon.core.arch.map
 import io.github.sophon.core.arch.mapError
-import io.github.sophon.core.domain.model.Hero
+import io.github.sophon.core.domain.model.Item
 import io.github.sophon.deadlock.data.DeadlockWikiDataSource
 import io.github.sophon.deadlock.data.mapper.toDomain
 
-internal class DownloadHeroesUseCase(
+internal class DownloadItemsUseCase(
     private val source: DeadlockWikiDataSource,
 ) {
-    suspend fun invoke(): Result<List<Hero>, WikiError> {
-        return source.downloadHeroList()
+    suspend fun invoke(): Result<List<Item>, WikiError> {
+        return source.downloadItemList()
             .map { map -> map.entries.map { it.toDomain() } }
             .mapError { it.toDomain() }
     }
