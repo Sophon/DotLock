708721a0cb6313251a0adfe025828a2936ef2c23
Le Duy Anh <lenduya@gmail.com>
2026-04-13 19:49:22 +0200

    feat (deadlock): fetch ability usecase

--- feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/DeadlockWikiClient.kt
+++ feat/deadlock/src/commonMain/kotlin/io/github/sophon/deadlock/DeadlockWikiClient.kt
@@ -7,6 +7,7 @@ import io.github.sophon.core.arch.WikiError
 import io.github.sophon.core.domain.model.Ability
 import io.github.sophon.core.domain.model.Hero
 import io.github.sophon.core.domain.model.Item
+import io.github.sophon.deadlock.usecase.FetchAbilityUseCase
 import io.github.sophon.deadlock.usecase.FetchHeroUseCase
 import io.github.sophon.deadlock.usecase.FetchItemUseCase
 import io.github.sophon.deadlock.usecase.SyncDataUseCase
@@ -25,6 +26,7 @@ internal class DeadlockWikiClientImpl(
     private val syncDataUseCase: SyncDataUseCase,
     private val fetchItemUseCase: FetchItemUseCase,
     private val fetchHeroUseCase: FetchHeroUseCase,
+    private val fetchAbilityUseCase: FetchAbilityUseCase,
 ): DeadlockWikiClient {
     override suspend fun downloadAllData(): EmptyResult<WikiError> {
         return syncDataUseCase.invoke()
@@ -35,7 +37,7 @@ internal class DeadlockWikiClientImpl(
     }
 
     override suspend fun fetchAbility(abilityName: String): Result<Ability, WikiError> {
-        TODO("Not yet implemented")
+        return fetchAbilityUseCase.invoke(abilityName)
     }
 
     override suspend fun fetchAbilities(heroName: String): Result<List<Ability>, WikiError> {
