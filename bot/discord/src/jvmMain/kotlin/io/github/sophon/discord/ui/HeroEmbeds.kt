e3e538ecb9775f2bdd1a4a0c75a6d34005999a38
Le Duy Anh <lenduya@gmail.com>
2026-04-13 17:58:02 +0200

    feat (discord): hero embeds

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/ui/HeroEmbeds.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/ui/HeroEmbeds.kt
@@ -4,6 +4,7 @@ import dev.kord.common.Color
 import dev.kord.rest.builder.message.EmbedBuilder
 import io.github.sophon.core.domain.model.FeatureInfo
 import io.github.sophon.core.domain.model.Hero
+import io.github.sophon.core.domain.model.Weapon
 
 internal fun heroEmbed(
     hero: Hero,
@@ -13,7 +14,60 @@ internal fun heroEmbed(
     color = Color(BEIGE)
     featureFooter(featureInfo)
 
-    //TODO:
+    weaponSection(hero.weapon)
+    vitalitySection(hero)
+    abilitySection(hero.boundAbilities)
+}
+
+
+private fun EmbedBuilder.weaponSection(weapon: Weapon) {
+    mandatoryField(
+        name = "Bullets",
+        value = buildString {
+            append("- DMG: ${weapon.bulletDamage}\n")
+            append("- SPD: ${weapon.bulletSpeed}\n")
+            append("- pSHT: ${weapon.bulletsPerShot}; pBRST: ${weapon.bulletsPerBurst}\n")
+        },
+    )
+    mandatoryField(
+        name = "Clip",
+        value = buildString {
+            append("- Size: ${weapon.clipSize}\n")
+            append("- Reload: ${weapon.reloadTime}\n")
+        }
+    )
+}
+
+private fun EmbedBuilder.vitalitySection(hero: Hero) {
+    mandatoryField(
+        name = "HP",
+        value = buildString {
+            append("- HP: ${hero.maxHealth}\n")
+            append("- regen: ${hero.baseHealthRegen}\n")
+            append("- Stamina: ${hero.stamina.stamina}\n")
+            append("- Stamina CD: ${hero.stamina.cooldown}\n")
+        }
+    )
+    mandatoryField(
+        name = "Movement",
+        value = buildString {
+            append("- Move: ${hero.movement.maxMoveSpeed}\n")
+            append("- Sprint: ${hero.movement.sprintSpeedMultiplier}\n")
+            append("- Dash: ${hero.dash.groundDashSpeed} (gnd); ${hero.dash.airDashSpeed} (air)\n")
+        }
+    )
+}
+
+private fun EmbedBuilder.abilitySection(abilityList: List<Hero.BoundAbility>) {
+    mandatoryField(
+        name = "Abilities",
+        value = buildString {
+            for (ability in abilityList) {
+                append("${ability.slot}. ${ability.name}\n")
+            }
+        },
+        inline = false,
+    )
 }
 
 
