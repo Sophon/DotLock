7bebe601ddff0c06b348616cfb0d561b54fee61a
Le Duy Anh <lenduya@gmail.com>
2026-04-14 17:55:53 +0200

    feat (discord): ability embeds

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/ui/AbilityEmbeds.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/ui/AbilityEmbeds.kt
@@ -13,7 +13,68 @@ internal fun abilityEmbed(
     color = Color(BEIGE)
     featureFooter(featureInfo)
 
-    //TODO:
+    targetingSection(ability.targeting)
+    timingSection(ability.timing)
+    propertiesField(ability.propertySet, ability.upgrades)
+}
+
+
+private fun EmbedBuilder.targetingSection(targeting: Ability.Targeting) {
+    optionalField(
+        name = "Cast range",
+        value = targeting.castRange?.value?.toString(),
+    )
+    optionalField(
+        name = "Unit targets",
+        value = targeting.unitTargetLimit?.toString()
+    )
+}
+
+private fun EmbedBuilder.timingSection(timing: Ability.Timing) {
+    mandatoryField(
+        name = "CD",
+        value = timing.cooldown?.value?.toString(),
+    )
+
+    if (timing.channelTime?.value != null) {
+        optionalField(
+            name = "Channel",
+            value = "${timing.channelTime?.value} (ms: ${timing.channelMoveSpeed})"
+        )
+    }
+
+    optionalField(
+        name = "Delay",
+        value = timing.castDelay?.toString()
+    )
+    optionalField(
+        name = "Charges",
+        value = timing.charges?.toString()
+    )
+}
+
+private fun EmbedBuilder.propertiesField(
+    propertySet: Set<Ability.Property>,
+    upgradeList: List<Ability.Upgrade>,
+) {
+    val bonuses = buildString {
+        propertySet.forEach { property ->
+            append("- ${property.type.name}: ${property.value.value}\n")
+        }
+        upgradeList.forEach { upgrade ->
+            val formattedValue: Double = when (val v = upgrade.changes.second) {
+                is Ability.Upgrade.UpgradeValue.Plain -> v.value
+                is Ability.Upgrade.UpgradeValue.Scaled -> v.scaledValue.value
+            }
+            append("- ${upgrade.changes.first}: $formattedValue")
+        }
+    }
+
+    mandatoryField(
+        name = "Bonuses",
+        value = bonuses,
+        inline = false,
+    )
 }
 
 
