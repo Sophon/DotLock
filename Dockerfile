0436a2590feba8b7835a8086919204c0dc9a0923
Le Duy Anh <lenduya@gmail.com>
2026-04-16 02:40:05 +0200

    v0.1.2

--- Dockerfile
+++ Dockerfile
@@ -33,4 +33,4 @@ RUN mkdir -p /app/res
 COPY res/config.json /app/res/config.json
 
 # Run the bot
-CMD ["java", "-Xms128m", "-Xmx192m", "-jar", "firefrog.jar"]
+CMD ["java", "-Xms256m", "-Xmx450m", "-jar", "firefrog.jar"]
--- bot/discord/build.gradle.kts
+++ bot/discord/build.gradle.kts
@@ -71,7 +71,7 @@ tasks.matching { it.name == "jvmRun" }.configureEach {
     (this as JavaExec).workingDir = rootProject.projectDir
 }
 
-val featureVersion = "0.1.1"
+val featureVersion = "0.1.2"
 buildkonfig {
     packageName = "io.github.sophon.discord"
 
